1fe8fcaa1b5762c768c095de0849b03ef5a57a26
lyorig <petrik.sacha@gmail.com>
2023-11-28 20:52:48 +0100

    updated main example

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <halcyon/mono_app.hpp>
 #include <iostream>
 
@@ -5,27 +6,31 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc == 1)
+    {
+        HAL_DEBUG_PRINT(hal::severity::error, "Missing argument. Exiting.");
+        return EXIT_FAILURE;
+    }
+
     hal::mono_app game { "Interloper 1.1" };
 
-    // game.mixer.mus.play("assets/ost/The Way Home.mp3", hal::infinite_loop);
+    game.mixer.mus.play("assets/ost/The Way Home.mp3", hal::infinite_loop);
 
-    const hal::font cnf { game.ttf.load_font("assets/fonts/JetBrains Mono.ttf", 72) },
-        txf { game.ttf.load_font("assets/fonts/m5x7.ttf", 144) };
-    const hal::texture tex { game.window, txf.render(argc == 1 ? "Made with Halcyon" : argv[1]) };
+    const hal::font    txf { game.ttf.load_font("assets/fonts/m5x7.ttf", 72) };
+    const hal::texture tex { game.window, txf.render(argv[1]).resize(game.window.size().x * 0.5 / txf.size_text(argv[1]).x) };
 
-    hal::texture dlt { game.window };
-
-    game.window.renderer.set_fill(hal::color::blue);
-
-    lyo::precise_timer tmr, delta;
+    hal::texture       dlt { game.window };
     hal::color         bg { hal::color::blue };
+    lyo::precise_timer tmr, delta;
+
+    game.window.renderer.set_fill(bg);
 
     while (game.update() && !game.input().pressed(hal::button::esc))
     {
         const std::string dt = std::to_string((0.01666666 / delta()) * 60.0);
 
         (dlt = txf.render(dt)).draw(hal::anchor::bottom_left);
-        // HAL_DEBUG_PRINT(hal::severity::info, dt, " FPS");  // <-- 40FPS without, 300 with?!
+        // HAL_DEBUG_PRINT(hal::severity::info, dt, " FPS");  // Why the fuck does this line double the FPS?
 
         delta.reset();
 
@@ -36,7 +41,7 @@ int main(int argc, char* argv[])
         bg.r = lyo::round_cast<lyo::u8>((sine + 1.0) * 64.0);
         game.window.renderer.set_fill(bg);
 
-        HAL_CONSOLE_DRAW(cnf, game.window);
+        HAL_CONSOLE_DRAW(txf, game.window);
     }
 
     return EXIT_SUCCESS;
