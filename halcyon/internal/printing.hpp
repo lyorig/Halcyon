2ce19c4186c73dc07aefe9391a040817a2e99be0
lyorig <31075205+lyorig@users.noreply.github.com>
2023-12-25 11:36:13 +0100

    wording

--- halcyon/internal/printing.hpp
+++ halcyon/internal/printing.hpp
@@ -11,12 +11,12 @@ namespace hal
 {
     // (u)int8_t types get interpreted as characters. This tries to prevent that.
     template <lyo::arithmetic T>
-    using to_printable = std::conditional_t<sizeof(T) == 1, std::conditional_t<std::is_signed_v<T>, lyo::i16, lyo::u16>, T>;
+    using to_printable_int = std::conditional_t<sizeof(T) == 1, std::conditional_t<std::is_signed_v<T>, lyo::i16, lyo::u16>, T>;
 
     template <lyo::arithmetic T>
     std::ostream& operator<<(std::ostream& str, point<T> pt)
     {
-        using print_type = to_printable<T>;
+        using print_type = to_printable_int<T>;
 
         return str << '[' << print_type(pt.x) << ',' << print_type(pt.y) << ']';
     }
