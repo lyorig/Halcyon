37b6e729ec02a7119bbbe4486360b1e4ada97094
lyorig <31075205+lyorig@users.noreply.github.com>
2023-12-26 20:03:33 +0100

    info -> init print

--- halcyon/image_loader.cpp
+++ halcyon/image_loader.cpp
@@ -10,8 +10,7 @@ image_loader::image_loader(engine& eng, il<image_type> types)
     HAL_DEBUG_ASSERT_VITAL(::IMG_Init(il2bm<int>(types)) == il2bm<int>(types),
         ::IMG_GetError());
 
-    HAL_DEBUG_PRINT("Initialized image loader with flags ",
-        il2bm<int>(types));
+    HAL_DEBUG_PRINT(hal::debug::init, "Initialized image loader with flags ", il2bm<int>(types));
 }
 
 image_loader::~image_loader() { ::IMG_Quit(); }
