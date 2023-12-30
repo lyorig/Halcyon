a320b96bbe4289c019200c67397fc3a42862a523
lyorig <31075205+lyorig@users.noreply.github.com>
2023-12-30 03:15:27 +0100

    Master include file

---
+++ halcyon/halcyon.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <halcyon/ecs.hpp>
+#include <halcyon/engine.hpp>
+#include <halcyon/image_loader.hpp>
+#include <halcyon/input_handler.hpp>
+#include <halcyon/mixer.hpp>
+#include <halcyon/texture.hpp>
+#include <halcyon/ttf_engine.hpp>
+#include <halcyon/window.hpp>
+
+// halcyon.hpp:
+// A convenience "include-everything" header file.
