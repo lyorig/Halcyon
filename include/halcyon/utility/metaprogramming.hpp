b3239c0a89facdf2b28c7c11404db05e376b578e
lyorig <petrik.sacha@gmail.com>
2024-05-06 22:32:23 +0200

    Type list

--- include/halcyon/utility/metaprogramming.hpp
+++ include/halcyon/utility/metaprogramming.hpp
@@ -40,18 +40,40 @@ namespace hal
         template <std::size_t I, typename What, typename... Ts>
         struct index_of<I, What, What, Ts...>
         {
-            constexpr static std::size_t value = I;
+            constexpr static std::size_t value { I };
         };
     }
 
     template <typename What, typename... Where>
-    constexpr inline bool is_present_v = (std::is_same_v<What, Where> || ...);
+    constexpr inline bool is_present_v { (std::is_same_v<What, Where> || ...) };
 
+    // Get the type residing at an index in a parameter pack.
     template <std::size_t I, typename... Ts>
         requires(I < sizeof...(Ts))
     using type_at_t = detail::type_at<0, I, Ts...>::type;
 
+    // Get the index at which a type resides in a parameter pack that contains it.
+    // This will report the index of the first occurrence.
     template <typename What, typename... Where>
         requires is_present_v<What, Where...>
-    constexpr inline std::size_t index_of_v = detail::index_of<0, What, Where...>::value;
+    constexpr inline std::size_t index_of_v { detail::index_of<0, What, Where...>::value };
+
+    // A holder, of sorts, of a parameter pack.
+    // Provides basic functionality.
+    template <typename... Ts>
+    struct type_list
+    {
+        constexpr static std::size_t size { sizeof...(Ts) };
+
+        template <typename T>
+        constexpr static std::size_t index_of { index_of_v<T, Ts...> };
+
+        template <std::size_t N>
+        using at_index = type_at_t<N, Ts...>;
+
+        // Wrap the provided type list in a type that takes a parameter pack.
+        // Hopefully, that's understandable to you, 'cause it sure ain't to me.
+        template <template <typename...> typename T>
+        using wrap = T<Ts...>;
+    };
 }
